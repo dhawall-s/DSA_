c00ddde3d3b1dab9dd5c1c31467d0d07824015d8
dhawall-s <sainidhawal755@gmail.com>
2025-10-31 16:53:38 +0530

    first commit

--- pointers/Pointer_1.cpp
+++ pointers/Pointer_1.cpp
@@ -3,10 +3,25 @@ using namespace std;
 int main()
 {
     int num = 5;
-    int *ptr = &num;
-    cout << num << endl;
-    cout << *ptr << endl;
-    cout << ptr << endl;
-    cout<< &num <<endl ;
+    num = num + 1;
+    int *ptr = &num; // p is a pointer to int
+    (*ptr)++;
+    cout << "Num is:" << num << endl;
+    cout << "*ptr is:" << *ptr << endl; // value at address of num
+    cout << "ptr is:" << ptr << endl;   // value of address of num
+    cout << "&num is:" << &num << endl; // address of num
+    char ch = 'D';
+    char *p = &ch;
+    cout << "ch is:" << ch << endl;
+    cout << "*p is " << *p << endl;
+    cout << "p is :" << p << endl;
+    double d = 4.5050;
+    double *pd = &d;
+    cout << "d is:" << d << endl;
+    cout << "*p is " << *pd << endl;
+    cout << "p is :" << pd << endl;
+    cout << "&d is :" << &d << endl;
+    cout << "Size of pointer is :" << sizeof(*pd) << endl;
+    cout << "Size of double is :" << sizeof(d) << endl;
     return 0;
 }
