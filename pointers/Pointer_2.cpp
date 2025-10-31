b099422f3f1cd2acec41da62ae35155c067d396d
dhawall-s <sainidhawal755@gmail.com>
2025-10-31 22:05:45 +0530

    first commit

---
+++ pointers/Pointer_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    cout << arr << endl; // address of first memory block is 1st addresss of array initialisation
+    cout << &arr[0] << endl;
+    cout << "Value will print at arr[0]:" << arr[0] << endl;
+    cout << &arr[1] << endl;
+    cout << "size of arr[0] : " << sizeof(arr[0]) << endl;
+    cout << *arr + 10 << endl;  // First location mein 1 add hoga
+    cout << *(arr + 1) << endl; // Second Location Print Karega
+    
+
+    return 0;
+}
