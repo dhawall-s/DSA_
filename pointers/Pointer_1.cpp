022e7f61fb94982c844eac171c57cfae43cf1212
dhawall-s <sainidhawal755@gmail.com>
2025-10-31 16:36:26 +0530

    first commit

---
+++ pointers/Pointer_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 5;
+    int *ptr = &num;
+    cout << num << endl;
+    cout << *ptr << endl;
+    cout << ptr << endl;
+    cout<< &num <<endl ;
+    return 0;
+}
